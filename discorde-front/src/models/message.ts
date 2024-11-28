5efbb20cfd3559044ed72619bdf1c9f1b2a4e28e
bultauzor <183804440+bultauzor@users.noreply.github.com>
2024-11-28 16:43:56 +0100

    add slash commands

--- discorde-front/src/models/message.ts
+++ discorde-front/src/models/message.ts
@@ -5,13 +5,16 @@ export class Message {
   author: string
   message: string
   timestamp$ = new BehaviorSubject<string>("now")
+  message$
 
 
   constructor(timestamp: number, author: string, message: string) {
     this.timestamp = new Date(timestamp);
     this.author = author;
     this.message = message;
+    this.message$ = new BehaviorSubject<string>(message)
     setInterval(() => this.timestamp$.next(this.formatTimestamp()), 30)
+    this.parseMessage()
   }
 
   private formatTimestamp() {
@@ -30,6 +33,23 @@ export class Message {
       return `1 minute ago`
     } else {
       return "now"
+    }
+  }
+
+  private parseMessage() {
+    if (this.message.startsWith("/")) {
+      switch (this.message.substring(1).split(" ")[0]) {
+        case "tableflip":
+          this.message$.next("(╯°□°)╯︵ ┻━┻")
+          break
+        case "type":
+          const msg = this.message.substring(6)
+          for (let i = 0; i <= msg.length; i++) {
+            setTimeout(() => {
+              this.message$.next(msg.substring(0, i))
+            }, 200 * (i + 1))
+          }
+      }
     }
   }
 }
