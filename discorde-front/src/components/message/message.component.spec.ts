afaeeadf29baa6d05c30f9a3492d71a14a52504d
bultauzor <183804440+bultauzor@users.noreply.github.com>
2024-11-28 17:34:53 +0100

    begining of test

--- discorde-front/src/components/message/message.component.spec.ts
+++ discorde-front/src/components/message/message.component.spec.ts
@@ -1,6 +1,8 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MessageComponent} from './message.component';
+import {Message} from '../../models/message';
+import {input} from '@angular/core';
 
 describe('MessageComponent', () => {
   let component: MessageComponent;
@@ -20,4 +22,12 @@ describe('MessageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be correct', async () => {
+    const msg = new Message(Date.now() - 3600000, "michel", "foo")
+    // @ts-ignore
+    component.timestamp = input(await msg.timestamp$.toPromise())
+    fixture.detectChanges();
+    // check if '1 minute ago'
+  })
 });
